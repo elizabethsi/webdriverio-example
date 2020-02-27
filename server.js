d514ce2b26339ef7ce5c06952fd74c1258697ea0
James Spence <jspence@crescendocollective.com>
2020-02-27 17:38:44 -0600

    now using routes and pug to serve up front end

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const Koa  = require('koa');
 const koaStatic = require('koa-static');
 const dotenv = require('dotenv');
+const koaViews = require('koa-views');
+const Router = require('@koa/router');
 
 dotenv.config();
 
@@ -8,6 +10,16 @@ const rootDir = __dirname + '/public';
 
 const app = new Koa();
 app.use(koaStatic(rootDir));
+app.use(koaViews(`${__dirname}/views`, {
+  extension: 'pug'
+}));
+
+const router = new Router();
+router.get('/', ctx => ctx.render('index'));
+router.get('/foo', ctx => ctx.render('foo'));
+
+app.use(router.routes());
+app.use(router.allowedMethods());
 
 const port = process.env.APP_PORT || 3000;
 app.listen(port, () => console.log(`App listening on port ${port}.`));
