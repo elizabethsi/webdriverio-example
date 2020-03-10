7e64ed477a93c6a287bd54cb4d9fe47f3d4eca0c
James Spence <jspence@crescendocollective.com>
2020-03-10 10:56:55 -0500

    reworked error styling on inputs

--- src/components/demo/Input.js
+++ src/components/demo/Input.js
@@ -2,11 +2,10 @@ import React from 'react';
 import './Input.scss';
 
 const Input = ({ name, label, placeholder = '', type = 'text', onChange, error = null }) => (
-  <div className="field">
+  <div className={`field ${error != null ? 'error' : ''}`}>
     <label htmlFor={name} className="label">
-      { label }
+      { label } {error != null && <small className="error-msg">({error})</small>}
     </label>
-    { error != null && <p className="error">{error}</p> }
     <div className="control">
       <input type={type} className="input" onChange={e => onChange(name, e.target.value)} id={name} name={name} placeholder={placeholder} />
     </div>
